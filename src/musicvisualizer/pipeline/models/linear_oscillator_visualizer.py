8ea2be13182340e77d9011b460aa46419412ca05
bkushigian <bkushigian@gmail.com>
2017-05-03 22:58:59 -0400

    Added colors to linear_oscillator_visualizer.py

--- src/musicvisualizer/pipeline/models/linear_oscillator_visualizer.py
+++ src/musicvisualizer/pipeline/models/linear_oscillator_visualizer.py
@@ -1,4 +1,5 @@
 from musicvisualizer.pipeline.ir import VisualizableMixin
+from musicvisualizer.utils.colors import ColorWheel
 from numpy import zeros
 import numpy as np
 from math import sin, cos, atan, pi
@@ -8,7 +9,6 @@ class LinearOscillatorVisualizer(VisualizableMixin):
         self.losc = linear_oscillator
         self.mode = mode
 
-
     def visualize(self, width = 720, height = 512):
         '''Return a list of numpy frames'''
         pt_list  = list(iter(self.losc))
@@ -17,13 +17,15 @@ class LinearOscillatorVisualizer(VisualizableMixin):
         if pt_width > len(pt_list[0]):
             raise Exception('width of video must be greater than width of points')
 
-        result   = []
+        result = []
 
+        cw = ColorWheel(pt_width)
         # Handle mode == 'dots
         if self.mode == 'dots':
             for points in pt_list:
                 arr = zeros(shape = (height, width, 3), dtype=np.uint8)
                 for i, (point, vel) in enumerate(points):
+                    R,G,B = cw(i)
                     # Weird offsets are to allow a 2x2 pixel (+/- 1...)
                     x = int(i * (width-2) / pt_width + 2)
                     # HEY! So the following y value works like this:
@@ -35,35 +37,35 @@ class LinearOscillatorVisualizer(VisualizableMixin):
                         y = height - 2
                     if y < 1:
                         y = 1
-                    arr[y][x][0]     = 255
-                    arr[y-1][x][0]   = 255
-                    arr[y-2][x][0]   = 255
-                    arr[y][x-1][0]   = 255
-                    arr[y-1][x-1][0] = 255
-                    arr[y-2][x-1][0] = 255
-                    arr[y][x-2][0]   = 255
-                    arr[y-1][x-2][0] = 255
-                    arr[y-2][x-2][0] = 255
+                    arr[y][x][0]     = R
+                    arr[y-1][x][0]   = R
+                    arr[y-2][x][0]   = R
+                    arr[y][x-1][0]   = R
+                    arr[y-1][x-1][0] = R
+                    arr[y-2][x-1][0] = R
+                    arr[y][x-2][0]   = R
+                    arr[y-1][x-2][0] = R
+                    arr[y-2][x-2][0] = R
 
-                    arr[y][x][1]     = 255
-                    arr[y-1][x][1]   = 255
-                    arr[y-2][x][1]   = 255
-                    arr[y][x-1][1]   = 255
-                    arr[y-1][x-1][1] = 255
-                    arr[y-2][x-1][1] = 255
-                    arr[y][x-2][1]   = 255
-                    arr[y-1][x-2][1] = 255
-                    arr[y-2][x-2][1] = 255
+                    arr[y][x][1]     = G
+                    arr[y-1][x][1]   = G
+                    arr[y-2][x][1]   = G
+                    arr[y][x-1][1]   = G
+                    arr[y-1][x-1][1] = G
+                    arr[y-2][x-1][1] = G
+                    arr[y][x-2][1]   = G
+                    arr[y-1][x-2][1] = G
+                    arr[y-2][x-2][1] = G
 
-                    arr[y][x][2]     = 255
-                    arr[y-1][x][2]   = 255
-                    arr[y-2][x][2]   = 255
-                    arr[y][x-1][2]   = 255
-                    arr[y-1][x-1][2] = 255
-                    arr[y-2][x-1][2] = 255
-                    arr[y][x-2][2]   = 255
-                    arr[y-1][x-2][2] = 255
-                    arr[y-2][x-2][2] = 255
+                    arr[y][x][2]     = B
+                    arr[y-1][x][2]   = B
+                    arr[y-2][x][2]   = B
+                    arr[y][x-1][2]   = B
+                    arr[y-1][x-1][2] = B
+                    arr[y-2][x-1][2] = B
+                    arr[y][x-2][2]   = B
+                    arr[y-1][x-2][2] = B
+                    arr[y-2][x-2][2] = B
 
                 result.append(arr)
 
@@ -72,6 +74,7 @@ class LinearOscillatorVisualizer(VisualizableMixin):
             for points in pt_list:
                 arr = zeros(shape = (height, width, 3), dtype=np.uint8)
                 for i, (point, vel) in enumerate(points):
+                    R,G,B = cw(i)
                     # Weird offsets are to allow a 2x2 pixel (+/- 1...)
                     x = int(i * (width-2) / pt_width + 2)
                     # HEY! So the following y value works like this:
@@ -87,31 +90,31 @@ class LinearOscillatorVisualizer(VisualizableMixin):
                     mid = height / 2
                     if y > mid:
                         while y > mid:
-                            arr[y][x][0]     = 255
-                            arr[y][x-1][0]   = 255
-                            arr[y][x-2][0]   = 255
+                            arr[y][x][0]     = R
+                            arr[y][x-1][0]   = R
+                            arr[y][x-2][0]   = R
 
-                            arr[y][x][1]     = 255
-                            arr[y][x-1][1]   = 255
-                            arr[y][x-2][1]   = 255
+                            arr[y][x][1]     = G
+                            arr[y][x-1][1]   = G
+                            arr[y][x-2][1]   = G
 
-                            arr[y][x][2]     = 255
-                            arr[y][x-1][2]   = 255
-                            arr[y][x-2][2]   = 255
+                            arr[y][x][2]     = B
+                            arr[y][x-1][2]   = B
+                            arr[y][x-2][2]   = B
                             y -= 1
                     else:
                         while y < mid:
-                            arr[y][x][0]     = 255
-                            arr[y][x-1][0]   = 255
-                            arr[y][x-2][0]   = 255
+                            arr[y][x][0]     = R
+                            arr[y][x-1][0]   = R
+                            arr[y][x-2][0]   = R
 
-                            arr[y][x][1]     = 255
-                            arr[y][x-1][1]   = 255
-                            arr[y][x-2][1]   = 255
+                            arr[y][x][1]     = G
+                            arr[y][x-1][1]   = G
+                            arr[y][x-2][1]   = G
 
-                            arr[y][x][2]     = 255
-                            arr[y][x-1][2]   = 255
-                            arr[y][x-2][2]   = 255
+                            arr[y][x][2]     = B
+                            arr[y][x-1][2]   = B
+                            arr[y][x-2][2]   = B
                             y += 1
                 result.append(arr)
 
